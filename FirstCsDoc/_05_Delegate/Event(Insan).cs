233ab6f3baf553f24e9c6b2c1d28a0939c6df957
pasaismihan <pasa_13142@hotmail.com>
2023-06-08 17:12:45 +0300

    Base Class Updated

--- FirstCsDoc/_05_Delegate/Event(Insan).cs
+++ FirstCsDoc/_05_Delegate/Event(Insan).cs
@@ -6,10 +6,14 @@ using System.Collections.Generic;
 
 namespace _05_DelegateEvent
 {
-	internal class Event_Insan_
-	{
-		static void Main(string[] arg)
+   
+    public class Event_Insan_
+    {
+        public delegate void OlayTipi();
+        public static void Main(string[] arg)
 		{
+
+
 			Insan pasa = new Insan("PASA");
 			Insan mustafa = new Insan("MUSTAFA");
 			Insan osman = new Insan("OSMAN");
@@ -26,8 +30,25 @@ namespace _05_DelegateEvent
 
 			Console.Read();
 
-			 
-		}
+
+			// BURADA ISCI ISVEREN ORNEGINI AYAGA KALDIRIYORUZ (SENARYOYU YAZIYORUZ)
+			Isveren isveren = new Isveren();
+			Isci[] isciler = new Isci[10];
+			for (int i = 0; i < isciler.Length; i++)
+			{
+				isciler[i] = new Isci(i + 1);
+							isveren.IsGeldi += isciler[i].Calis;
+				isciler[i].IseBasladim += isveren.BasladikBilgisiVer;
+				isciler[i].IsinOrtasindayim += isveren.OrtasindayizBilgisiVer;
+				isciler[i].IsiBitirdim += isveren.UrununuzHazir;
+               
+            }
+            isveren.IsDagit();
+            Console.Read();
+
+
+
+        }
 	}
 }
 
