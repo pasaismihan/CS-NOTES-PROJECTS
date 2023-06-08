ebdc7e82beafa1a38de585f8c30f8814b9fa95b2
pasaismihan <pasa_13142@hotmail.com>
2023-06-08 17:14:20 +0300

    Event Example Done

---
+++ FirstCsDoc/_05_Delegate/Isci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static _05_DelegateEvent.Event_Insan_;
+
+namespace _05_DelegateEvent
+{
+	public class Isci
+	{
+        public event OlayTipi IseBasladim;
+        public event OlayTipi IsinOrtasindayim;
+        public event OlayTipi IsiBitirdim;
+        public int Sicil { get; set; }
+        public Isci(int sicil)
+        {
+            Sicil = sicil; 
+        }
+
+        public void Calis()
+        {
+            Console.WriteLine("{0}.sicildeki personelim ve ise basladim",Sicil);
+            Thread.Sleep(2000);
+            IseBasladim();
+            Console.WriteLine("{0}.sicildeki personelim ve isin ortasindayim",Sicil);
+            Thread.Sleep(2000);
+            IsinOrtasindayim();
+            Console.WriteLine("{0}.sicildeki personelim ve isi bitirdim",Sicil);
+            Thread.Sleep(2000);
+            IsiBitirdim();
+            Console.Read();
+        }
+    }
+}
+
---
+++ FirstCsDoc/_05_Delegate/Isveren.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static _05_DelegateEvent.Event_Insan_;
+
+namespace _05_DelegateEvent
+{
+	public class Isveren
+	{
+		//public delegate void OlayTipi();
+		public event OlayTipi IsGeldi;
+
+		public void BasladikBilgisiVer()
+		{
+			Console.WriteLine("sayin musterimiz urununuz icin calismaya basladik");
+		}
+		public void OrtasindayizBilgisiVer()
+		{
+			Console.WriteLine("sayin musterimiz urununuz kisa surede hazir olacaktir");
+		}
+		public void UrununuzHazir()
+		{
+			Console.WriteLine("sayin musterimiz urununuz hazir");
+		}
+		public void IsDagit()
+		{
+			IsGeldi();
+		}
+	}
+}
+
