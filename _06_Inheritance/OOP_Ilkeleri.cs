3ba333dae26c59bdec07c89a7dfdc5b42362599d
pasaismihan <pasa_13142@hotmail.com>
2023-06-10 20:22:08 +0300

    oop principles done

---
+++ _06_Inheritance/OOP_Ilkeleri.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace _06_Inheritance
+{
+	public class Inheritance
+	{
+		public static void Main(string[] arg)
+		{
+            //OOP ILKELERI - Asagida verilen oop ilkeleri oop ozelligini tasiyan her programlama dilinde olmak zorundadir!
+
+            /*Inheritance - kalitim anlamina gelir . birden fazla class da benzer kodlar yapmak yerine bunlar icin ortak ust class olusturuyoruz bunun icinde kodlarimizi toplayip
+            ilgili classlara dagitiyoruz*/
+
+            /*Encapsulation - butun cikislari kapatip bir tane kapi birakiyoruz disariya , burada access modifiers lardan bahsedebiliriz. Bu sekilde kontrollu erisim saglayabiliriz.
+            field ve property ikilisinde de bu ilke vardir field da private olarak kapali kullaniriz daha sonra property ile cikis kapisi birakiriz bu bir encapsulation ornegidir*/
+
+            /*Polymorphism - cok bicimlilik demektir.child classlara member degistirme izni verir.
+            buna ornek olarak futbolculari ele alabiliriz hepsi futbol oynar fakat kabiliyetleri ve diger ozellikleri farklidir
+            bir diger ornek 3 cesit kus ele alalim(civciv,karga,marti). kuslarin hepsinin otmesi farkli olacagi icin base class da ortak bir sey tanimlariz daha sonra child class larda
+            bu otme icin verilen memberi(ornegin cik cik) ezerek devam ederiz buna polymorphism denir. Polymorphism olusturmak icin keyword kullanacagiz ilerleyen orneklerde...*/
+
+            /*Abstraction - soyutlama demektir. Polymorphism in bir koludur. Kodu olmayan soyut yapilardir, ornek nefes al methodu olusturacagiz ama bunun icerisini doldurmayacagiz
+            abstraction da kus ornegini baz alirsak base class da bir member tanimlamiyoruz child classlarda tanimlama yapacagimizi soyluyoruz polymorphism den farki budur*/
+
+
+
+        }
+    }
+}
+
